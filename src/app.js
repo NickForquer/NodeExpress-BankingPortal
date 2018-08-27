4a9a408f5ab7cbda18ed3dbb0e9b43bbd5c7f1c5
Forquer, Nick <nick.forquer@avanade.com>
2018-08-27 15:52:37 -0400

    completed module 5

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+
 
 const app = express();
 
@@ -12,36 +15,9 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/savings', (req, res) => {
-    res.render('account', {account: accounts.savings})
-} );
-app.get('/checking', (req, res) => {
-    res.render('account', {account: accounts.checking})
-} );
-app.get('/credit', (req, res) => {
-    res.render('account', {account: accounts.credit})
-} );
+app.use('/account', accountRoutes);
 
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer')
-});
-app.post('/transfer', (req,res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', {message: "Transfer Completed"});
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {account: accounts.credit})});
-
-app.post('/payment', (req, res)=> {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
-})
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) =>{
      res.render('index', {title: 'Account Summary', accounts})});
